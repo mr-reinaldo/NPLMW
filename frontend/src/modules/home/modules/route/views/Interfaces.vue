7251de68da827d6e1b61dcb74d0412ca521357fa
Reinaldo Silva <josereinaldo.pessoal@gmail.com>
2023-12-10 22:23:07 -0300

    Corrigindo erro ao buscar interfaces de rede

--- frontend/src/modules/home/modules/route/views/Interfaces.vue
+++ frontend/src/modules/home/modules/route/views/Interfaces.vue
@@ -169,7 +169,7 @@ const getInterfaces = async () => {
     state.value.isLoading = false
 
     // Filter interfaces in data
-    interfaces.value = napalmStore.data.interfaces
+    interfaces.value = napalmStore.data.data.interfaces
 
     //Get interfaces names
     for (const [key, value] of Object.entries(interfaces.value)) {
